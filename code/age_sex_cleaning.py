1627c1cc38c9b62b1866c146832e8af9651067d9
bburt26 <jtg3ts@virginia.edu>
2025-11-18 21:58:58 +0000

    Bacheler's age/sex w rangling fixes

--- code/age_sex_cleaning.py
+++ code/age_sex_cleaning.py
@@ -1,27 +1,21 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Load the dataset
 data = pd.read_csv('/workspaces/Final-Project/data/CVD Dataset.csv')
 
-# Keep only Age and Sex
-columns_of_interest = ['Age', 'Sex']
+columns_of_interest = ['Age', 'Sex', 'Systolic BP', 'Diastolic BP']
 data_cleaned = data[columns_of_interest].copy()
 
-# Replace missing values with NaN
 data_cleaned = data_cleaned.replace({None: np.nan, '': np.nan})
-data_cleaned = data_cleaned.fillna(np.nan)
+data_cleaned = data_cleaned.dropna()
 
-# One-hot encode Sex column
 data_cleaned = pd.get_dummies(data_cleaned, columns=['Sex'], drop_first=True)
 
-# coerce
-data_cleaned['Age'] = pd.to_numeric(data_cleaned['Age'], errors='coerce').astype('Int64')
+# Convert Age to int64
+data_cleaned['Age'] = pd.to_numeric(data_cleaned['Age'], errors='coerce').astype('int64')
+
+# Convert encoded Sex column to int64
 data_cleaned['Sex_M'] = data_cleaned['Sex_M'].astype('int64')
 
-
-# Save cleaned data
-data_cleaned.to_csv('CVD_Cleaned.csv', index=False)
+data_cleaned.to_csv('/workspaces/Final-Project/code/CVD_Cleaned.csv', index=False)
 
