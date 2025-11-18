f2d46f94220570cf168bace1cdf1f4953cb70a4f
bburt26 <jtg3ts@virginia.edu>
2025-11-18 21:54:26 +0000

    Bacheler's wrangling for age/sex

--- code/age_sex_cleaning.py
+++ code/age_sex_cleaning.py
@@ -17,6 +17,11 @@ data_cleaned = data_cleaned.fillna(np.nan)
 # One-hot encode Sex column
 data_cleaned = pd.get_dummies(data_cleaned, columns=['Sex'], drop_first=True)
 
+# coerce
+data_cleaned['Age'] = pd.to_numeric(data_cleaned['Age'], errors='coerce').astype('Int64')
+data_cleaned['Sex_M'] = data_cleaned['Sex_M'].astype('int64')
+
+
 # Save cleaned data
 data_cleaned.to_csv('CVD_Cleaned.csv', index=False)
 
